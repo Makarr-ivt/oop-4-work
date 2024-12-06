12e99a71c87230d11f52cdeae94a95309424074a
Makarr <maanromanov@edu.hse.ru>
2024-12-06 23:36:40 +0300

    unit tests v0 created

---
+++ unit_tests/tests.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+int run_all_tests();
+int test_default_constructor();
+int test_copy_constructor();
+int test_change_in_size();
+int test_add_operator();
+int test_intersection();
+int test_file_data_exchange();
+// юнит тесты -- это автоматические тесты, которые тестируют конкретные части программы:
+// модули или функции в частности.
+// есть типа ещё юзер тесты или типа того, которые ты вручную делаешь

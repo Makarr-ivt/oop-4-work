e9d4c7057b12820b3c777596521d98805429b83f
Makarr <maanromanov@edu.hse.ru>
2024-12-07 23:04:01 +0300

    template tests

--- entry_point/main.cpp
+++ entry_point/main.cpp
@@ -6,24 +6,9 @@ using namespace std;
 #include "HashTableV1.hpp"
 
 int main() {
-    if (run_all_tests<HashTableV1<string, int>>() != 0) {
+    if (run_all_tests<HashTableV1<string, string>>() != 0) {
         return 1;
     }
-    HashTableV1<string, int> ht;
-    ht.insert("first", 3);
-    ht.insert("second", 14);
-    ht.insert("third", 3);
-    cout << ht.get_size() << endl;
-    cout << ht.is_contains("first") << endl;
-    cout << ht.is_empty() << endl;
-    HashTableV1<string, int> ht2(ht);
-    ht.clear();
-    cout << ht.is_contains("first") << endl;
-    cout << ht.is_empty() << endl;
-    cout << ht2.is_contains("first") << endl;
-    ht2.remove("first");
-    cout << ht2.is_contains("first") << endl;
-    cout << ht2.is_empty() << endl;
-
+    
     return 0;
 }
--- unit_tests/tests.hpp
+++ unit_tests/tests.hpp
@@ -1,53 +1,91 @@
 #pragma once
-#include <cassert>
 #include "HashTableV1.hpp"
+#include "HashTableV2.hpp"
+#include <cassert>
 #include <iostream>
 using namespace std;
 
 
-template<typename T> // Hashtable V1 or V2
+template<typename T> // template for HashTable V1 or V2
 int test_default_constructor() {
+    assert(T example
+    && "default constructor fails!\n");
+    assert(example.get_size() == 0
+    && "default constructor creates wrong start size!\n");
+    assert(example.is_empty() == true
+    && "created constructor is not empty!");
     return 0;
 }
 
-template<typename T> // Hashtable V1 or V2
+template<typename T> // template for HashTable V1 or V2
 int test_copy_constructor() {
+    T ht; 
+    ht.insert("first key", "first value"); // да, по факту я тестирую только вариант {string : string}
+    ht.insert("second", "");
+    ht.insert("third", "something else");
+    T ht2(ht);
+    assert(ht == ht2
+    && "copied hashtable must be equal to original!");
+
+    ht2.remove("second");
+    assert(!(ht == ht2) // here i also test operator==
+    && "change in copy must not modify original!");
+
+    ht.clear();
+    assert(!(ht == ht2) // here i also test operator==
+    && "change in original must not modify copy!");
     return 0;
 }
 
-template<typename T> // Hashtable V1 or V2
+template<typename T> // template for HashTable V1 or V2
 int test_change_in_size() {
+    T ht;
+    ht.insert("1", "11");
+    assert(ht.get_size() == 1
+    && "size must be 1 here!");
+
+    ht.insert("2", "22");
+    ht.insert("3", "33");
+    assert(ht.get_size() == 3
+    && "size must be 3 here!");
+
+    ht.remove("2");
+    assert(ht.get_size() == 2
+    && "size must be 2 here!");
+
+    ht.clear();
+    assert(ht.get_size() == 0
+    && "size must be 0 here!");
+    
+    assert(ht.is_empty() == true
+    && "hashtable must be empty after clearing!");
+    
     return 0;
 }
 
-template<typename T> // Hashtable V1 or V2
+template<typename T> // template for HashTable V1 or V2
 int test_add_operator() {
     return 0;
 }
 
-template<typename T> // Hashtable V1 or V2
-int test_equality_operator() {
-    return 0;
-}
 
-template<typename T> // Hashtable V1 or V2
+template<typename T> // template for HashTable V1 or V2
 int test_intersection() {
     return 0;
 }
 
-template<typename T> // Hashtable V1 or V2
+template<typename T> // template for HashTable V1 or V2
 int test_file_data_exchange() {
     return 0;
 }
 
-template<typename T> // Hashtable V1 or V2
+template<typename T> // template for HashTable V1 or V2
 int run_all_tests() {
     int fails_counter = 0;
     fails_counter += test_default_constructor<T>();
     fails_counter += test_copy_constructor<T>();
     fails_counter += test_change_in_size<T>();
     fails_counter += test_add_operator<T>();
-    fails_counter += test_equality_operator<T>();
     fails_counter += test_intersection<T>();
     fails_counter += test_file_data_exchange<T>();
     cout << fails_counter << " tests were failed!\n";
